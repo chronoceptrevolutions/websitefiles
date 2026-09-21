a253441e227521f1072944d9bef7ed20d3eadbd6
chronoceptrevolutions <chronoceptrevolutions@gmail.com>
2026-09-21 15:11:22 -0400

    Update home.js

--- assets/home.js
+++ assets/home.js
@@ -115,7 +115,7 @@ function wireSubscribeForm() {
         name: document.getElementById("subName").value.trim(),
         email: document.getElementById("subEmail").value.trim(),
         country: document.getElementById("subCountry").value.trim(),
-        age: document.getElementById("subAge").value,
+        age_group: document.getElementById("subAge").value,
       });
       if (error) throw error;
 
